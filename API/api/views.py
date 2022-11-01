92583c14a229680369424eef33b0cb7788335079
patilaniket04@gmail.com <patilaniket04@gmail.com>
2022-11-01 19:03:26 +0530

    added basic authentication in APIs

--- API/api/views.py
+++ API/api/views.py
@@ -4,11 +4,21 @@ from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, D
 # from .models import Lead, UserProfile,User
 from .serializers import StudentSerializer
 from .models import Student
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 # from rest_framework import status
 # # Create your views here.
 class StudentListView(ListAPIView):
     serializer_class = StudentSerializer
     queryset = Student.objects.all()
+# ------------------Added basic authentication--------------------------
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+#----- -----------Any one can access the api -------------------------
+#     permission_classes = [AllowAny]
+# # ------------------Only Staff user status ia active an access ---------------
+#     permission_classes = [IsAdminUser]
+
 
 class StudentCreateView(CreateAPIView):    
     queryset = Student.objects.all()
